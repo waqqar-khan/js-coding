d7e7ef3c5f0584e9da721b88b46fd06c037e1b03
Wick119 <waqqarkhan248@gmail.com>
2022-07-22 11:32:55 +0530

    Reverse a string problem solution

---
+++ reverseString.js
@@ -0,0 +1,30 @@
+// PROBLEM STATEMENT
+// Given a string, return a new string with the reversed order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('awesome') === 'emosewa'
+
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverse("apple"));
+
+// 2nd way
+// function reverse(str){
+//     return str.split('').reduce((rev, char) => char + rev,'');
+// }
+
+// console.log(reverse('hello'));
+
+// 3rd way
+// function reverse(str){
+//     let rev = '';
+//     for(let char of str){
+//         rev = char + rev;
+//     }
+//     return rev;
+// }
+
+// console.log(reverse('awesome'));
